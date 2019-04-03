645cc154d4e96fae2ac463c41cf81e9fe45d5ed8
James Coglan <jcoglan@gmail.com>
2019-04-03 09:03:47 +0100

    More glue: generating image data in Rust and writing it to a canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 
+
 #[wasm_bindgen]
 pub struct Scene;
 
@@ -9,7 +10,48 @@ impl Scene {
         Scene
     }
 
-    pub fn hello(&self) -> String {
-        String::from("hello world")
+    pub fn render(&self, image: &mut Image) {
+        for x in 0 .. image.width {
+            for y in 0 .. image.height {
+                image.put(x, y, RGB(255, 0, 0));
+            }
+        }
+    }
+}
+
+
+struct RGB(u8, u8, u8);
+
+
+#[wasm_bindgen]
+pub struct Image {
+    width: usize,
+    height: usize,
+    pixels: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl Image {
+    pub fn new(width: usize, height: usize) -> Self {
+        let size = width * height * 4;
+        let pixels = vec![0; size];
+        Image { width, height, pixels }
+    }
+
+    pub fn as_ptr(&self) -> *const u8 {
+        self.pixels.as_ptr()
+    }
+
+    pub fn len(&self) -> usize {
+        self.pixels.len()
+    }
+
+    fn put(&mut self, x: usize, y: usize, color: RGB) {
+        let RGB(r, g, b) = color;
+        let idx = (x + y * self.width) * 4;
+
+        for (i, &v) in [r, g, b, 255].iter().enumerate() {
+            self.pixels[idx + i] = v;
+        }
     }
 }
--- web/index.js
+++ web/index.js
@@ -1,5 +1,15 @@
-import { Scene } from "../pkg/ray_tracer"
+import { Image, Scene } from "../pkg/ray_tracer"
 import { memory } from "../pkg/ray_tracer_bg"
 
+
+let canvas = document.getElementById("viewer"),
+    ctx    = canvas.getContext("2d")
+
+let image  = Image.new(canvas.width, canvas.height),
+    pixels = new Uint8ClampedArray(memory.buffer, image.as_ptr(), image.len()),
+    data   = new ImageData(pixels, canvas.width, canvas.height)
+
 let scene = Scene.new()
-alert(scene.hello())
+
+scene.render(image)
+ctx.putImageData(data, 0, 0)
