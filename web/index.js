d6342dd01148b170197332add1c1b4c4f88c0c54
James Coglan <jcoglan@gmail.com>
2019-05-23 14:30:04 +0100

    Update the example scene to include many more small spheres.

--- web/index.js
+++ web/index.js
@@ -15,7 +15,22 @@ scene.add_sphere(Pt.new(-18, 0,  60), 10, RGB.new(0xf7, 0x5c, 0x03))
 scene.add_sphere(Pt.new(  0, 0, 120), 10, RGB.new(0x22, 0x74, 0xa5))
 scene.add_sphere(Pt.new( 18, 0,  60), 10, RGB.new(0xf1, 0xc4, 0x0f))
 
-scene.add_point_light(Pt.new(-9, -20, 90), 500.0)
+for (let d = 0; d < 360; d += 20) {
+  let rad = d * Math.PI / 180,
+      t   = 60 * Math.PI / 180,
+      r   = 15
+
+  for (let ox of [-18, 18]) {
+    let x = ox + r * Math.sin(rad) * Math.cos(t),
+        y =  0 + r * Math.sin(rad) * Math.sin(t),
+        z = 60 + r * Math.cos(rad)
+
+    scene.add_sphere(Pt.new(x, y, z), 1.4, RGB.new(0x33, 0xff, 0xcc))
+  }
+}
+
+scene.add_point_light(Pt.new(-9, -20, 90), 900)
+scene.add_point_light(Pt.new(28, 20, 40), 400)
 
 scene.render(image)
 ctx.putImageData(data, 0, 0)
